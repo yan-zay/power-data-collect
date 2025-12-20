e5bd849bba5f265759caf016690266f43abda5f3
zhou_ay <yan_zay@163.com>
2025-12-20 17:37:47 +0800

    调整项目目录结构 1.2.0

--- power-data-collect-core/src/main/java/com/dtxytech/powerdatacollect/core/PowerDataCollectCoreApplication.java
+++ power-data-collect-core/src/main/java/com/dtxytech/powerdatacollect/core/PowerDataCollectCoreApplication.java
@@ -1,13 +1,13 @@
-package com.dtxytech.powerdatacollect.core;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class PowerDataCollectCoreApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(PowerDataCollectCoreApplication.class, args);
-    }
-
-}
+//package com.dtxytech.powerdatacollect.core;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class PowerDataCollectCoreApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(PowerDataCollectCoreApplication.class, args);
+//    }
+//
+//}
