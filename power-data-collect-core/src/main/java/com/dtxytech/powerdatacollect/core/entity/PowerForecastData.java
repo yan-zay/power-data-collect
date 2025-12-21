078bb7781f11000abb183021a213ebe1b4b17bd7
zhou_ay <yan_zay@163.com>
2025-12-21 20:17:28 +0800

    优化代码 1.2.3

--- power-data-collect-core/src/main/java/com/dtxytech/powerdatacollect/core/entity/PowerForecastData.java
+++ power-data-collect-core/src/main/java/com/dtxytech/powerdatacollect/core/entity/PowerForecastData.java
@@ -14,7 +14,6 @@ import java.util.List;
  * @Author zay
  * @Date 2025/12/15 20:29
  */
-
 @Data
 @TableName(value = "sne_power_forecast_data", autoResultMap = true) // ⭐ 必须开启 autoResultMap
 @Builder
@@ -42,11 +41,6 @@ public class PowerForecastData {
 
     private String createTime;
 
-    // ⭐ 核心：使用 JacksonTypeHandler 自动映射 JSON ↔ List<Point>
     @TableField(value = "forecast_data", typeHandler = JacksonTypeHandler.class)
-    private List<String> forecastData; // 也可以是 List<String>，但你示例是对象数组
-
-    // 如果你坚持用 List<String>，则需确保 JSON 是字符串数组：["a","b"]
-    // @TableField(typeHandler = JacksonTypeHandler.class)
-    // private List<String> forecastData;
+    private List<String> forecastData;
 }
